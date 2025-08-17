346b7a51457ed635d6cacac2d894b4b838eb5bfd
ParipoornaBhat <157298696+ParipoornaBhat@users.noreply.github.com>
2025-08-18 00:55:16 +0530

    TEMP: LIMIT R1 FUNCTIONS TO 5

--- src/lib/functionMeta.ts
+++ src/lib/functionMeta.ts
@@ -91,11 +91,11 @@ export const R1_FUNCTION_META: FunctionMeta[] = [
   { id: "R1Q3", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["any"], outputType: "string" },//done
   { id: "R1Q4", category: "Math", icon: Function, description: "Final solution function", numInputs: 2, inputTypes: ["array","string"], outputType: "string" },//DONE
   { id: "R1Q5", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["array"], outputType: "number" },//DONE
-  { id: "R1Q6", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { id: "R1Q7", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { id: "R1Q8", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { id: "R1Q9", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { id: "R1Q10", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { id: "R1Q6", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { id: "R1Q7", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { id: "R1Q8", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { id: "R1Q9", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { id: "R1Q10", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "number" },
 ];
 
 
