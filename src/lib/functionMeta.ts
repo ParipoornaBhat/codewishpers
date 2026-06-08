1eddbefdbc99fd07500cbca4fa96339da7f3ae25
Paripoorna <paripoornadhruva@gmail.com>
2026-06-08 09:43:56 +0530

    refac:Function Order

--- src/lib/functionMeta.ts
+++ src/lib/functionMeta.ts
@@ -40,30 +40,51 @@ export type SolutionFunctionMeta = {
 // ----------------- Main Function Metadata -----------------
 
 export const FUNCTION_META: FunctionMeta[] = [
-  { fc:"fn1", id: "fn1", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "number" },
-  { fc:"fn2", id: "fn2", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "float" },
-  { fc:"fn3", id: "fn3", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "float" },// for Q1
-  { fc:"fn4", id: "fn4", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "array" },
-  { fc:"fn5", id: "fn5", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
-  { fc:"fn6", id: "fn6", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "boolean" },// for Q2
-  { fc:"fn7", id: "fn7", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
-  { fc:"fn8", id: "fn8", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
-  { fc:"fn9", id: "fn9", category: "Logic", icon: Function, description: "", numInputs: 2, inputTypes: ["array","array"], outputType: "array" },// for Q3
-  { fc:"fn10", id: "fn10", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },// for Q4 + fn1
-  { fc:"fn11", id: "fn11", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { fc:"fn12", id: "fn12", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { fc:"fn13", id: "fn13", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },// for Q5 fn(11+ 11 + 12) OR fn(13 + 11)
-  { fc:"fn14", id: "fn14", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { fc:"fn15", id: "fn15", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { fc:"fn16", id: "fn16", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
-  { fc:"fn17", id: "fn17", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },//Q6 Sol Fn(13+14+15) OR Fn(13+16+17)
-  { fc:"fn18", id: "fn18", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
-  { fc:"fn19", id: "fn19", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
-  { fc:"fn20", id: "fn20", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "number" },// for Q7 + Q9
-  { fc:"fn21", id: "fn21", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "array" },
-  { fc:"fn22", id: "fn22", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" }, // for Q8
+    // { fc:"fn1", id: "fn1", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "number" },
+  // { fc:"fn2", id: "fn2", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "float" },
+  // { fc:"fn3", id: "fn3", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "float" },// for Q1
+  // { fc:"fn4", id: "fn4", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "array" },
+  // { fc:"fn5", id: "fn5", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  // { fc:"fn6", id: "fn6", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "boolean" },// for Q2
+  // { fc:"fn7", id: "fn7", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  // { fc:"fn8", id: "fn8", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  // { fc:"fn9", id: "fn9", category: "Logic", icon: Function, description: "", numInputs: 2, inputTypes: ["array","array"], outputType: "array" },// for Q3
+  // { fc:"fn10", id: "fn10", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },// for Q4 + fn1
+  // { fc:"fn11", id: "fn11", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { fc:"fn12", id: "fn12", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { fc:"fn13", id: "fn13", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },// for Q5 fn(11+ 11 + 12) OR fn(13 + 11)
+  // { fc:"fn14", id: "fn14", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { fc:"fn15", id: "fn15", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { fc:"fn16", id: "fn16", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  // { fc:"fn17", id: "fn17", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },//Q6 Sol Fn(13+14+15) OR Fn(13+16+17)
+  // { fc:"fn18", id: "fn18", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  // { fc:"fn19", id: "fn19", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  // { fc:"fn20", id: "fn20", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "number" },// for Q7 + Q9
+  // { fc:"fn21", id: "fn21", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "array" },
+  // { fc:"fn22", id: "fn22", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" }, // for Q8
 
-  
+  { fc:"fn15", id: "fn1", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "number" },
+  { fc:"fn22", id: "fn2", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "float" },
+  { fc:"fn20", id: "fn3", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "float" },// for Q1
+  { fc:"fn21", id: "fn4", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "array" },
+  { fc:"fn17", id: "fn5", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  { fc:"fn16", id: "fn6", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "boolean" },// for Q2
+  { fc:"fn13", id: "fn7", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  { fc:"fn18", id: "fn8", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  { fc:"fn19", id: "fn9", category: "Logic", icon: Function, description: "", numInputs: 2, inputTypes: ["array","array"], outputType: "array" },// for Q3
+  { fc:"fn11", id: "fn10", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },// for Q4 + fn1
+  { fc:"fn3", id: "fn11", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  { fc:"fn1", id: "fn12", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  { fc:"fn4", id: "fn13", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },// for Q5 fn(11+ 11 + 12) OR fn(13 + 11)
+  { fc:"fn2", id: "fn14", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  { fc:"fn9", id: "fn15", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  { fc:"fn10", id: "fn16", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },
+  { fc:"fn7", id: "fn17", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "number" },//Q6 Sol Fn(13+14+15) OR Fn(13+16+17)
+  { fc:"fn8", id: "fn18", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  { fc:"fn5", id: "fn19", category: "Math", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" },
+  { fc:"fn6", id: "fn20", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "number" },// for Q7 + Q9
+  { fc:"fn14", id: "fn21", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["number"], outputType: "array" },
+  { fc:"fn12", id: "fn22", category: "Logic", icon: Function, description: "", numInputs: 1, inputTypes: ["array"], outputType: "array" }, // for Q8
    // include solution function meta here too
   { fc:"Q001S", id: "Q001S", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["array"], outputType: "float" },
   { fc:"Q002S", id: "Q002S", category: "Math", icon: Function, description: "Final solution function", numInputs: 1, inputTypes: ["number"], outputType: "boolean" },
